a84035e57921090211a4fc42fbfc7ec083eb9b85
sadikhossain69 <sadikhossain433@gmail.com>
2023-03-13 20:44:43 +0600

    homepage main part done

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Header from '@/components/Header'
+import SquigglyLines from '@/components/SquigglyLines'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +17,56 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
+      <Header />
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-28 mt-20">
+        <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-slate-900 sm:text-7xl">
+          Restoring old photos{" "}
+          <span className="relative whitespace-nowrap text-[#3290EE]">
+            <SquigglyLines />
+            <span className="relative">using AI</span>
+          </span>{" "}
+          for everyone.
+        </h1>
+        <p className="mx-auto mt-12 max-w-xl text-lg text-slate-700 leading-7">
+          Have old and blurry face photos? Let our AI restore them so those
+          memories can live on. 100% free – restore your photos today.
+        </p>
+        <div className="flex justify-center space-x-4">
+
+          <Link
+            className="bg-black rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-black/80"
+            href="/restore"
+          >
+            Restore your photos
+          </Link>
+        </div>
+        <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
+          <div className="flex flex-col space-y-10 mt-4 mb-16">
+            <div className="flex sm:space-x-2 sm:flex-row flex-col">
+              <div>
+                <h2 className="mb-1 font-medium text-lg">Original Photo</h2>
+                <Image
+                  alt="Original photo of my bro"
+                  src="/michael.jpg"
+                  className="w-96 h-96 rounded-2xl"
+                  width={400}
+                  height={400}
+                />
+              </div>
+              <div className="sm:mt-0 mt-8">
+                <h2 className="mb-1 font-medium text-lg">Restored Photo</h2>
+                <Image
+                  alt="Restored photo of my bro"
+                  width={400}
+                  height={400}
+                  src="/michael-new.jpg"
+                  className="w-96 h-96 rounded-2xl sm:mt-0 mt-2"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   )
 }
